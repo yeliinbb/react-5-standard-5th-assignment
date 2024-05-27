e8dd9438e54e257acd38f9607279c54345a94150
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-05-27 22:15:32 +0900

    코드 수정

--- src/context/TextContextProvider.jsx
+++ src/context/TextContextProvider.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const TextContext = createContext(null);
 
 const TextContextProvider = ({ children }) => {
-  const [texts, setTexts] = useState(
+  const [texts, setTexts] = useState(() =>
     localStorage.getItem("texts")
       ? JSON.parse(localStorage.getItem("texts"))
       : []
