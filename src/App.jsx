f04aa829d4caa0ad234352cabaafca7ac4cc061e
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-05-29 15:30:38 +0900

    context 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import TextInput from "./components/TextInput";
 import TextList from "./components/TextList";
-import TextContextProvider from "./context/TextContextProvider";
 
 function App() {
   // TODO: texts 를 context api 로 리팩터링 하세요.
 
   return (
-    <TextContextProvider>
-      <div>
-        <h1>Text Input and Listing</h1>
-        <TextInput />
-        <TextList />
-      </div>
-    </TextContextProvider>
+    <div>
+      <h1>Text Input and Listing</h1>
+      <TextInput />
+      <TextList />
+    </div>
   );
 }
 
--- src/context/TextContextProvider.jsx
+++ src/context/TextContextProvider.jsx
@@ -1,8 +1,10 @@
 import { createContext, useEffect, useState } from "react";
 
+// ()안에 들어가는 값은 초기값
 export const TextContext = createContext(null);
 
 const TextContextProvider = ({ children }) => {
+  // text랑 관련있는 로직은 모두 context API로 이동
   const [texts, setTexts] = useState(() =>
     localStorage.getItem("texts")
       ? JSON.parse(localStorage.getItem("texts"))
